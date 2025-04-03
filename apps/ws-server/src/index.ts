7f8a2a198f27b172ebf629898d95e32b6b07c075
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-04-03 11:28:26 +0530

    add boilerplate express and ws conde

---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,15 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  ws.on("error", console.error);
+
+  ws.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
