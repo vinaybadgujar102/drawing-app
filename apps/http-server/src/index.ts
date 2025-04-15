ce452e80f703ed2397ea544b5862978d837b1d25
Vinay Badgujar <vinaybadgujar8@gmail.com>
2025-04-15 22:57:08 +0530

    added routes

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -4,40 +4,121 @@ import { middleware } from "./middleware";
 import { JWT_SECRET, PORT } from "@repo/backend-common/config";
 import {
   CreateUserSchema,
-  SignInSchema,
+  SigninSchema,
   CreateRoomSchema,
 } from "@repo/common/types";
 
+import bcrypt from "bcrypt";
+
+import { prisma } from "@repo/db/client";
 // do zod validation later
 
 const app = express();
 
-app.post("/signup", (req, res) => {
-  const parsedData = CreateUserSchema.safeParse(req.body);
+app.use(express.json());
 
-  if (!parsedData.success) {
-    res.status(400).json({ error: parsedData.error });
+app.post("/signup", async (req, res) => {
+  try {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+
+    if (!parsedData.success) {
+      res.status(400).json({ error: parsedData.error });
+      return;
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: parsedData.data.email,
+      },
+    });
+
+    if (user) {
+      res.status(400).json({ error: "User already exists" });
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(parsedData.data.password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: parsedData.data.email,
+        password: hashedPassword,
+        name: parsedData.data.name,
+      },
+    });
+
+    res.json({
+      userId: newUser.id,
+    });
+  } catch (error) {
+    res.status(400).json({ error: "Failed to create user" });
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  try {
+    const parsedData = SigninSchema.safeParse(req.body);
+    if (!parsedData.success) {
+      res.status(400).json({ error: parsedData.error });
+      return;
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: parsedData.data.email,
+      },
+    });
+
+    if (!user) {
+      res.status(400).json({ error: "User not found" });
+      return;
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      parsedData.data.password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      res.status(400).json({ error: "Invalid password" });
+      return;
+    }
+
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET);
+
+    res.json({ token });
+  } catch (error) {
+    res.status(400).json({ error: "Invalid email or password" });
     return;
   }
-
-  res.json({
-    userId: 1,
-  });
 });
 
-app.post("/signin", (req, res) => {
-  const userId = 1;
-  const token = jwt.sign({ userId }, JWT_SECRET);
+app.post("/room", middleware, async (req, res) => {
+  try {
+    const parsedData = CreateRoomSchema.safeParse(req.body);
 
-  res.json({ token });
-});
+    if (!parsedData.success) {
+      res.status(400).json({ error: parsedData.error });
+      return;
+    }
 
-app.post("/room", middleware, (req, res) => {
-  // db call
+    // @ts-ignore
+    const userId = req.userId;
 
-  res.json({
-    roomId: 1,
-  });
+    const room = await prisma.room.create({
+      data: {
+        slug: parsedData.data.name,
+        adminId: userId,
+      },
+    });
+
+    // db call
+    res.json({
+      roomId: room.id,
+    });
+  } catch (error) {
+    res.status(400).json({ error: "Failed to create room" });
+  }
 });
 
 app.listen(PORT, () => {
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,16 +1,16 @@
-import { z } from "zod";
+import z from "zod";
 
 export const CreateUserSchema = z.object({
-  username: z.string().min(1),
-  password: z.string().min(1),
-  name: z.string().min(1),
+  email: z.string().email(),
+  password: z.string(),
+  name: z.string(),
 });
 
-export const SignInSchema = z.object({
-  username: z.string().min(1),
-  password: z.string().min(1),
+export const SigninSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
 });
 
 export const CreateRoomSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(3).max(20),
 });
